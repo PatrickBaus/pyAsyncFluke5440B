168ae4d6ebb8db31c5bce7adf0a3ed01d26ee985
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2023-04-24 15:33:46 +0200

    Updated the self-test error message to include the service manual.

--- fluke5440b_async/errors.py
+++ fluke5440b_async/errors.py
@@ -19,6 +19,9 @@ class SelftestError(Fluke5440bError):
     """The device returned an error during self-test. See page 4-8 of the service manual for details."""
 
     def __init__(self, selftest: str, error_code: SelfTestErrorCode):
-        super().__init__(f"{selftest} self-test failed with error: {error_code}.")
+        super().__init__(
+            f"{selftest} self-test failed with error: {error_code}."
+            f" Check the service manual on page 4-8 for a solution."
+        )
 
         self.code = error_code
