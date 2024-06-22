0085bcaa4140b0462e47d45930fc47d61863024e
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2024-06-22 14:52:33 +0200

    Blacked files

--- fluke5440b_async/__init__.py
+++ fluke5440b_async/__init__.py
@@ -2,6 +2,7 @@
 This is an asyncIO library for the Fluke 5440B calibrator. It manages all functions of the calibrator and takes care
 of the internal state.
 """
+
 from ._version import __version__
 from .enums import DeviceState, ErrorCode, ModeType, SeparatorType, TerminatorType
 from .flags import SerialPollFlags, SrqMask, StatusFlags
